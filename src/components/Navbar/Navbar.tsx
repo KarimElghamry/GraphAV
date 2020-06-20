ac7de0a3fd14f4d387eb31a1e6db6f3a7a54afaa
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 18:05:45 +0200

    change: name Slider to ThemeSwitch

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import LabelText from './LabelText';
 import Row from '../common/Row';
 import GithubLogo from './GithubLogo';
 import Star from './Star';
-import Slider from './Slider/Slider';
+import ThemeSwitch from './ThemeSwitch/ThemeSwitch';
 
 interface NavbarProps {
   changeTheme: Function;
@@ -15,7 +15,7 @@ const Navbar: React.FC<NavbarProps> = (props: NavbarProps): ReactElement => {
     <Container>
       <LabelText>Graph Algorithms Visualizer</LabelText>
       <Row justifyContent="space-between" margin="0 18px">
-        <Slider changeTheme={props.changeTheme}></Slider>
+        <ThemeSwitch changeTheme={props.changeTheme}></ThemeSwitch>
         <GithubLogo></GithubLogo>
         <Star></Star>
       </Row>
---
+++ src/components/Navbar/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,27 @@
+import React, {ReactElement} from 'react';
+import Track from './Track';
+import Thumb from './Thumb';
+import themes from '../../../themes';
+import {Theme} from '../../../models/Theme';
+
+interface ThemeSwitchProps {
+  changeTheme: Function;
+}
+
+const ThemeSwitch: React.FC<ThemeSwitchProps> = (
+  props: ThemeSwitchProps
+): ReactElement => {
+  const handleChangeTheme: () => void = () => {
+    props.changeTheme((prev: Theme) =>
+      prev.name === 'dark' ? themes.light : themes.dark
+    );
+  };
+
+  return (
+    <Track onClick={() => handleChangeTheme()}>
+      <Thumb></Thumb>
+    </Track>
+  );
+};
+
+export default ThemeSwitch;
