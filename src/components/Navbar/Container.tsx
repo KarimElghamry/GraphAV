4cbf8d00054bb78cca9a6cf43adcf65d89728aaf
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 16:41:37 +0200

    add: navbar Container

---
+++ src/components/Navbar/Container.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import {Theme} from '../../models/Theme';
+
+interface Props {
+  theme: Theme;
+}
+
+const Container = styled.div<Props>`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${(props) => props.theme.navbar.background};
+  color: ${(props) => props.theme.navbar.foreground};
+`;
+
+export default Container;
