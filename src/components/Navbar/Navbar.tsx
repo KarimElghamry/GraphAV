eb0345b8cb623fe2d1a360d5065f2fbd2463c85d
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 17:28:27 +0200

    add: Slider file

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,12 +4,14 @@ import LabelText from './LabelText';
 import Row from '../common/Row';
 import GithubLogo from './GithubLogo';
 import Star from './Star';
+import Slider from './Slider/Slider';
 
 const Navbar: React.FC = (): ReactElement => {
   return (
     <Container>
       <LabelText>Graph Algorithms Visualizer</LabelText>
       <Row justifyContent="space-between" margin="0 18px">
+        <Slider></Slider>
         <GithubLogo></GithubLogo>
         <Star></Star>
       </Row>
---
+++ src/components/Navbar/Slider/Slider.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const Slider: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Slider;
