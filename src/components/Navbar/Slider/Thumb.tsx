0934af68b29bfab9dbf563ef6593dc87b40a5f58
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 17:55:21 +0200

    fix: thunb styling

--- src/components/Navbar/Slider/Thumb.tsx
+++ src/components/Navbar/Slider/Thumb.tsx
@@ -3,18 +3,16 @@ import {Theme} from '../../../models/Theme';
 
 interface Props {
   theme: Theme;
-  type?: string;
 }
 
 const Thumb = styled.div<Props>`
   position: absolute;
-  left: ${(props) => (props.type === 'dark' ? '0' : 'none')};
-  right: ${(props) => (props.type === 'dark' ? 'none' : '0')};
+  left: ${(props) => (props.theme.name === 'dark' ? '0' : 'calc(100% - 20px)')};
   height: 20px;
   width: 20px;
   border-radius: 20px;
   background-color: ${(props) => props.theme.navbar.foreground};
-  transition-duration: 0.5s;
+  transition-duration: 0.3s;
 `;
 
 export default Thumb;
