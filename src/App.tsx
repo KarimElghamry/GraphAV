b903e6140cdcdd0c031672ce1558d8316636437f
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 17:57:20 +0200

    fix: globalTheme state

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React, {ReactElement, useState} from 'react';
 import Home from './components/Home/Home';
 import {ThemeProvider} from 'styled-components';
 import themes from './themes';
-import Test from './components/Test/Test';
+import {Theme} from './models/Theme';
 
 const App: React.FC<{}> = (): ReactElement => {
-  const [globalTheme, setGlobalTheme] = useState<string>('dark');
+  const [globalTheme, setGlobalTheme] = useState<Theme>(themes.dark);
 
   return (
-    <ThemeProvider theme={globalTheme === 'dark' ? themes.dark : themes.light}>
+    <ThemeProvider theme={globalTheme}>
       <Home></Home>
     </ThemeProvider>
   );
