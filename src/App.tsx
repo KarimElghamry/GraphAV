a0e3210a9fc9e07545d6799785aa02ff4f9f6287
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 16:33:40 +0200

    add: Test component for theme testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React, {ReactElement, useState} from 'react';
 import Home from './components/Home/Home';
 import {ThemeProvider} from 'styled-components';
 import themes from './themes';
-import {Theme} from './models/Theme';
+import Test from './components/Test/Test';
 
 const App: React.FC<{}> = (): ReactElement => {
-  const [globalTheme, setGlobalTheme] = useState<Theme>(themes.dark);
+  const [globalTheme, setGlobalTheme] = useState<string>('dark');
 
   return (
-    <ThemeProvider theme={globalTheme}>
+    <ThemeProvider theme={globalTheme === 'dark' ? themes.dark : themes.light}>
       <Home></Home>
+      <Test toggleTheme={setGlobalTheme}></Test>
     </ThemeProvider>
   );
 };
