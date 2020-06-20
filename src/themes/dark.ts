c42e6ba779903d95f5d1a0d42466cdc94dd666af
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 15:44:11 +0200

    add: dark theme object

---
+++ src/themes/dark.ts
@@ -0,0 +1,30 @@
+import {Theme} from '../models/Theme';
+
+const dark: Theme = {
+  navbar: {
+    background: '#02E095',
+    foreground: 'white',
+  },
+  sidebar: {
+    background: 'white',
+    foreground: '#0D1929',
+  },
+  canvas: {
+    background: '#0D1929',
+    foreground: 'white',
+  },
+  nodeActive: {
+    background: '#02E095',
+    foreground: 'white',
+  },
+  nodeInactive: {
+    background: 'transparent',
+    foreground: 'white',
+  },
+  edge: {
+    background: 'white',
+    foreground: 'white',
+  },
+};
+
+export default dark;
