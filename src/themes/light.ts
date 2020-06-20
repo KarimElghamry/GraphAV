8582761a3cf71c79e5d6257c0f16a29aa87c9a8a
KarimElghamry <Karimelghamry@gmail.com>
2020-06-20 15:48:39 +0200

    add: light theme object

---
+++ src/themes/light.ts
@@ -0,0 +1,30 @@
+import {Theme} from '../models/Theme';
+
+const light: Theme = {
+  navbar: {
+    background: '#0D1929',
+    foreground: 'white',
+  },
+  sidebar: {
+    background: '#0D1929',
+    foreground: '#02E095',
+  },
+  canvas: {
+    background: '#EFEFEF',
+    foreground: '',
+  },
+  nodeActive: {
+    background: '#02E095',
+    foreground: 'white',
+  },
+  nodeInactive: {
+    background: '#0D1929',
+    foreground: 'white',
+  },
+  edge: {
+    background: '#0D1929',
+    foreground: 'white',
+  },
+};
+
+export default light;
