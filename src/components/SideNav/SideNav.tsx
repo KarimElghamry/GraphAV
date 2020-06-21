aea5aee7b661a9e0d77e66390d64584be6a49814
Ali-Amin <ali.m.amin98@gmail.com>
2020-06-21 11:49:57 +0200

    add: SideNav component

---
+++ src/components/SideNav/SideNav.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+import StyledSideNav from "./SideNavStyle"
+
+
+const SideNav: React.FC = (): ReactElement => {
+    return (
+        <StyledSideNav></StyledSideNav>
+    );
+};
+
+export default SideNav;
