d1da05b4e82444c6159153fa1bbc91239ea07263
KarimElghamry <Karimelghamry@gmail.com>
2020-06-21 13:17:40 +0200

    add: GraphCanvas file

---
+++ src/components/GraphCanvas/GraphCanvas.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const GraphCanvas: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default GraphCanvas;
