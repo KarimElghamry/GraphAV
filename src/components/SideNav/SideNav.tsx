1a92474c9e2fde530c64a659cebd3b1ead6b0959
Ali-Amin <ali.m.amin98@gmail.com>
2020-06-21 14:03:50 +0200

    add: SideNav/zoom text

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
-import StyledSideNav from "./SideNavStyle"
-import ZoomSlider from "./ZoomSlider"
+import StyledSideNav from "./SideNavStyle";
+import ZoomSlider from "./ZoomSlider";
+import ItemText from "./ItemText";
 
 const SideNav: React.FC = (): ReactElement => {
     return (
         <StyledSideNav>
+            <ItemText>Zoom</ItemText>
             <ZoomSlider>
                 <input type="range" className="slider" />
             </ZoomSlider>
