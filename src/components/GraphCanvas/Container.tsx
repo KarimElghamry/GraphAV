ea71c59306fd86a3b9b8a949fca7ea6c75ff3d29
KarimElghamry <Karimelghamry@gmail.com>
2020-06-21 13:23:27 +0200

    add: Node file

--- src/components/GraphCanvas/Container.tsx
+++ src/components/GraphCanvas/Container.tsx
@@ -6,6 +6,7 @@ interface Props {
 }
 
 const Container = styled.div<Props>`
+  position: relative;
   height: calc(100vh - 50px);
   width: 100%;
   background-color: ${(props) => props.theme.canvas.background};
---
+++ src/components/Node/Node.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const Node: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Node;
