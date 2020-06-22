807b50bb51c38aa925c737eba675f512a9a788eb
KarimElghamry <Karimelghamry@gmail.com>
2020-06-22 13:53:25 +0200

    add: isVisibile state to sideNav

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,18 +1,19 @@
-import React, { ReactElement } from 'react';
-import StyledSideNav from "./SideNavStyle";
-import ZoomSlider from "./ZoomSlider";
-import ItemText from "./ItemText";
+import React, {ReactElement, useState} from 'react';
+import StyledSideNav from './SideNavStyle';
+import ZoomSlider from './ZoomSlider';
+import ItemText from './ItemText';
 
 const SideNav: React.FC = (): ReactElement => {
-    return (
-        <StyledSideNav>
-            <ItemText>Zoom</ItemText>
-            <ZoomSlider>
-                <input type="range" className="slider" />
-            </ZoomSlider>
-        </StyledSideNav>
-    );
+  const [isVisible, setIsVisible] = useState<Boolean>();
+
+  return (
+    <StyledSideNav>
+      <ItemText>Zoom</ItemText>
+      <ZoomSlider>
+        <input type="range" className="slider" />
+      </ZoomSlider>
+    </StyledSideNav>
+  );
 };
 
 export default SideNav;
-
