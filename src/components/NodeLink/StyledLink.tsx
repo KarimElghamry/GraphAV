c059df777d33a0278c2f8c517c7805d932ad1f7b
Ali-Amin <ali.m.amin98@gmail.com>
2020-06-22 14:13:22 +0200

    add: Node Link styled component

---
+++ src/components/NodeLink/StyledLink.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+
+const StyledLink = styled.line`
+    stroke: ${(props) => props.theme.edge};
+    stroke-width: 3px;
+`;
+
+export default StyledLink;
