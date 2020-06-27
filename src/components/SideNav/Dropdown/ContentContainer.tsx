a3b9d26827512988f468ac163d02a021dc366e5f
KarimElghamry <Karimelghamry@gmail.com>
2020-06-27 15:36:35 +0200

    add: transition animation to dropdown content

--- src/components/SideNav/Dropdown/ContentContainer.tsx
+++ src/components/SideNav/Dropdown/ContentContainer.tsx
@@ -6,21 +6,19 @@ interface Props {
 }
 
 const ContentContainer = styled.div<Props>`
-  display: ${(props) => (props.isVisible ? 'block' : 'none')};
   overflow-y: scroll;
   overflow-x: hidden;
   position: absolute;
   top: 100%;
   width: 100%;
-  max-height: 135px;
+  max-height: ${(props) => (props.isVisible ? '135px' : '0px')};
+  border-radius: 0px 0px 12px 12px;
   background-color: ${(props) =>
     props.theme.name === 'dark'
       ? themes.light.navbar.background
       : themes.dark.navbar.background};
 
   transition-duration: 0.3s;
-  transition-property: background-color;
-
   scrollbar-color: white transparent;
   scrollbar-width: thin;
   &::-webkit-scrollbar {
