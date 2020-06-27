d6651e6c5806641c1c07189f9c64c58243b9fba2
KarimElghamry <Karimelghamry@gmail.com>
2020-06-27 14:11:38 +0200

    add: Dropdown file

---
+++ src/components/Navbar/Dropdown/Dropdown.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const Dropdown: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Dropdown;
