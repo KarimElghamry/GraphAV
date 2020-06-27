fc93cdac067b2400418e41dbae899bc2c6a09a64
KarimElghamry <Karimelghamry@gmail.com>
2020-06-27 18:17:09 +0200

    add: undirectedIcon to assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,14 @@
 import githubLogo from './images/gh-logo.png';
 import {ReactComponent as starIcon} from './images/star.svg';
 import {ReactComponent as addIcon} from './images/add.svg';
+import {ReactComponent as undirectedIcon} from './images/undirected.svg';
 
 const assets = {
   images: {
     githubLogo: githubLogo,
     starIcon: starIcon,
     addIcon: addIcon,
+    undirectedIcon: undirectedIcon,
   },
 };
 
