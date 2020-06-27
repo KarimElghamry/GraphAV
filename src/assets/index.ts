8f096607be993262a8a3d3d3756cd6b2c00f5913
KarimElghamry <Karimelghamry@gmail.com>
2020-06-27 17:49:13 +0200

    export: addIcon from assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,12 @@
 import githubLogo from './images/gh-logo.png';
 import {ReactComponent as starIcon} from './images/star.svg';
+import {ReactComponent as addIcon} from './images/add.svg';
 
 const assets = {
   images: {
     githubLogo: githubLogo,
     starIcon: starIcon,
+    addIcon: addIcon,
   },
 };
 
