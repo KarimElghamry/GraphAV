8d43296931bc551a462bec3d9bfbbf8f31a0628c
KarimElghamry <Karimelghamry@gmail.com>
2020-06-27 13:36:17 +0200

    add: adjacency list state

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,15 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement, useState} from 'react';
 import Navbar from '../Navbar/Navbar';
 import SideNav from '../SideNav/SideNav';
 import GraphCanvas from '../GraphCanvas/GraphCanvas';
-
 
 interface HomeProps {
   changeTheme: Function;
 }
 
 const Home: React.FC<HomeProps> = (props: HomeProps): ReactElement => {
+  const [adjacencyList, setAdjacencyList] = useState<Array<Array<number>>>([]);
+
   return (
     <div>
       <SideNav />
