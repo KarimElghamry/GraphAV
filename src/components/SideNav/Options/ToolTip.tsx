ee6635b8a24a7732439eb35b98fb258dc0cb85ad
KarimElghamry <Karimelghamry@gmail.com>
2020-06-28 16:27:30 +0200

    add: background styling for ToolTip

--- src/components/SideNav/Options/ToolTip.tsx
+++ src/components/SideNav/Options/ToolTip.tsx
@@ -1,16 +1,38 @@
 import styled from 'styled-components';
+import themes from '../../../themes';
 
 interface Props {
   isVisible: boolean;
 }
 
 const ToolTip = styled.div<Props>`
+  z-index: 999;
   position: absolute;
-  top: 110%;
-  width: 100px;
-  height: ${(props) => (props.isVisible ? '35px' : '0px')};
+  top: 115%;
+  width: 80px;
+  height: ${(props) => (props.isVisible ? '30px' : '0px')};
+  border-radius: 6px;
   color: white;
-  background-color: black;
+  background-color: ${(props) =>
+    props.theme.name === 'dark'
+      ? themes.light.navbar.background
+      : themes.dark.navbar.background};
+  transition-duration: 0.3s;
+
+  &::after {
+    content: ' ';
+    position: absolute;
+    top: -10px;
+    left: 50%;
+    margin-left: -5px;
+    border-width: ${(props) => (props.isVisible ? '5px' : '0px')};
+    border-style: solid;
+    border-color: transparent transparent
+      ${(props) =>
+        props.theme.name === 'dark'
+          ? themes.light.navbar.background
+          : themes.dark.navbar.background}; transparent;
+  }
 `;
 
 export default ToolTip;
