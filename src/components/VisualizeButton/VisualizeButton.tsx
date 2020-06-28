68516b215bdfe7120b34daccf50f86301931b4bf
KarimElghamry <Karimelghamry@gmail.com>
2020-06-28 16:59:52 +0200

    add: VisualizeButton file

---
+++ src/components/VisualizeButton/VisualizeButton.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const VisualizeButton: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default VisualizeButton;
