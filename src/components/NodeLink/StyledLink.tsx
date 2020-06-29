bf0bdbe5366f5b4e9fc9699a357974ab23c6abbb
Ali-Amin <ali.m.amin98@gmail.com>
2020-06-29 19:24:12 +0200

    add: transition-duration to edge

--- src/components/NodeLink/StyledLink.tsx
+++ src/components/NodeLink/StyledLink.tsx
@@ -3,8 +3,9 @@ import styled from "styled-components";
 
 
 const StyledLink = styled.line`
-    stroke: ${(props) => props.theme.edge};
+    stroke: ${(props) => props.theme.edge.background};
     stroke-width: 3px;
+    transition-duration: 0.3s
 `;
 
 export default StyledLink;
