b3be201001e0a75522057aa80eed9569911c7a23
KarimElghamry <Karimelghamry@gmail.com>
2020-07-04 16:33:25 +0200

    add: visualizationSpeed state

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -15,6 +15,7 @@ const Home: React.FC<HomeProps> = (props: HomeProps): ReactElement => {
   const [startingNode, setStartingNode] = useState<number>(0);
   const [isVisualizing, setIsVisualizing] = useState<boolean>(false);
   const [zoomPercentage, setZoomPercentage] = useState<number>(1);
+  const [visualizationSpeed, setVisualizationSpeed] = useState<number>(1);
   const [isConnecting, setIsConnecting] = useState<boolean>(false);
   const [edgeFirstNode, setEdgeFirstNode] = useState<number>();
 
