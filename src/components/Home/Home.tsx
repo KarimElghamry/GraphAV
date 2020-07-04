d6f077bd40b2314e03db6e5ebed74d58b62b8084
KarimElghamry <Karimelghamry@gmail.com>
2020-07-04 15:51:00 +0200

    remove: sampleGraph

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,17 +5,12 @@ import GraphCanvas from '../GraphCanvas/GraphCanvas';
 import VisualizeButton from '../VisualizeButton/VisualizeButton';
 import algorithms from '../../algorithms';
 
-//TODO: Remove hard coded graph
-const sampleGraph = [[1, 2, 4], [0, 3], [0, 5], [1], [0], [2]];
-
 interface HomeProps {
   changeTheme: Function;
 }
 
 const Home: React.FC<HomeProps> = (props: HomeProps): ReactElement => {
-  const [adjacencyList, setAdjacencyList] = useState<Array<Array<number>>>(
-    sampleGraph
-  );
+  const [adjacencyList, setAdjacencyList] = useState<Array<Array<number>>>([]);
   const [visited, setVisited] = useState<Array<number>>([]);
   const [startingNode, setStartingNode] = useState<number>(0);
   const [isVisualizing, setIsVisualizing] = useState<boolean>(false);
