20c8d7ca18a26ebf80aaa2141d5f096a0b40940f
KarimElghamry <Karimelghamry@gmail.com>
2020-07-04 17:01:23 +0200

    add: sidenav/ClearButton

---
+++ src/components/SideNav/ClearButton.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ClearButton = styled.div`
+  background-color: red;
+  color: white;
+  height: 30px;
+  border-radius: 10px;
+`;
+
+export default ClearButton;
