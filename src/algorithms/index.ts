b73709b5615be2683626d19b1725bd9b34427d87
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-05 19:00:31 +0200

    add: export for bfs algorithm

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,9 +1,11 @@
 import dfsWrapper from './dfs';
 import dijkstra from './dijkstra';
+import bfs from './bfs';
 
 const algorithms = {
   dfs: dfsWrapper,
   dijkstra: dijkstra,
+  bfs: bfs
 };
 
 export default algorithms;
