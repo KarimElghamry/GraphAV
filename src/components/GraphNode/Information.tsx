f06c6edcb5662f5cd18ce4ac2d8a78d0f964f753
KarimElghamry <Karimelghamry@gmail.com>
2020-07-11 14:50:14 +0200

    change: information style & disable pointer events

--- src/components/GraphNode/Information.tsx
+++ src/components/GraphNode/Information.tsx
@@ -1,24 +1,13 @@
 import styled from 'styled-components';
-import Position from '../../models/Position';
 
-interface Props {
-  position: Position;
-  width: number;
-}
-
-const Information = styled.div.attrs((props: Props) => ({
-  style: {
-    top: `${props.position.top}px`,
-    left: `${props.position.left}px`,
-  },
-}))<Props>`
-  width: ${(props) => `${props.width}px`};
+const Information = styled.div`
   position: absolute;
+  top: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  user-select: none;
+  pointer-events: none;
 `;
 
 export default Information;
