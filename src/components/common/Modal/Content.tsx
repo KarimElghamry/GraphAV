e8e91a224acff60cd1274d33fb5b09f7f541e9f3
KarimElghamry <Karimelghamry@gmail.com>
2020-07-11 18:07:07 +0200

    add: Modal/Content

---
+++ src/components/common/Modal/Content.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Content = styled.div`
+  min-width: 300px;
+  min-height: 150px;
+  border-radius: 20px;
+  background-color: white;
+`;
+
+export default Content;
