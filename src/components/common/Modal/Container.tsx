2d951a15f1d7c602c46eef5351393af91721acca
KarimElghamry <Karimelghamry@gmail.com>
2020-07-11 17:57:02 +0200

    add: Modal/Container

---
+++ src/components/common/Modal/Container.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  opacity: 0.5;
+`;
+
+export default Container;
