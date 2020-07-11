5bc3ab4e77676af2c3237f549050c1dbe6bd532c
KarimElghamry <Karimelghamry@gmail.com>
2020-07-11 14:22:13 +0200

    add: GraohNode/Information

---
+++ src/components/GraphNode/Information.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Information = styled.div`
+  position: absolute;
+  top: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export default Information;
