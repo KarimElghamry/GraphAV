5828c47ce5b8dc9ae3677253e06cd1fdd6b90aee
KarimElghamry <Karimelghamry@gmail.com>
2020-07-11 16:07:56 +0200

    fix: out of canvas bug when zooming

--- src/components/GraphNode/GraphNode.tsx
+++ src/components/GraphNode/GraphNode.tsx
@@ -1,4 +1,10 @@
-import React, {ReactElement, useState, useRef, useEffect} from 'react';
+import React, {
+  ReactElement,
+  useState,
+  useRef,
+  useEffect,
+  useCallback,
+} from 'react';
 import Container from './Container';
 import Position from '../../models/Position';
 import Information from './Information';
@@ -16,36 +22,48 @@ interface Props {
 const GraphNode: React.FC<Props> = (props: Props): ReactElement => {
   const [position, setPosition] = useState<Position>({top: 100, left: 100});
   const nodeRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  const infoRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
   const canvasRef: React.RefObject<HTMLDivElement> = props.canvasRef;
 
-  const handleMouseMove = (e: MouseEvent) => {
-    if (nodeRef.current !== null && canvasRef.current !== null) {
-      const nodeWidth: number = +nodeRef.current.offsetWidth;
-      const canvasWidth: number = +canvasRef.current.offsetWidth;
-      const canvasHeight: number = +canvasRef.current.offsetHeight;
-      const navbarHeight: number = canvasWidth < 700 ? 90 : 50;
+  const handleMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (
+        nodeRef.current !== null &&
+        canvasRef.current !== null &&
+        infoRef.current !== null
+      ) {
+        const nodeWidth: number = +nodeRef.current.offsetWidth;
+        const canvasWidth: number = +canvasRef.current.offsetWidth;
+        const canvasHeight: number = +canvasRef.current.offsetHeight;
+        const navbarHeight: number = canvasWidth < 700 ? 90 : 50;
+        const infoBoxHeight: number = +infoRef.current.offsetHeight;
 
-      let newLeft: number = e.clientX - nodeWidth / 2;
-      let newTop: number = e.clientY - navbarHeight - nodeWidth / 2;
+        let newLeft: number = e.clientX - nodeWidth / 2;
+        let newTop: number = e.clientY - navbarHeight - nodeWidth / 2;
 
-      if (e.clientX - nodeWidth / 2 <= 0) {
-        newLeft = 0;
-      } else if (e.clientX - nodeWidth / 2 >= canvasWidth - nodeWidth) {
-        newLeft = canvasWidth - nodeWidth;
+        if (e.clientX - nodeWidth / 2 <= 0) {
+          newLeft = 0;
+        } else if (e.clientX - nodeWidth / 2 >= canvasWidth - nodeWidth) {
+          newLeft = canvasWidth - nodeWidth;
+        }
+
+        if (e.clientY - navbarHeight - nodeWidth / 2 <= 0) {
+          newTop = 0;
+        } else if (
+          e.clientY - navbarHeight - nodeWidth / 2 >=
+          canvasHeight - nodeWidth - infoBoxHeight
+        ) {
+          newTop = canvasHeight - nodeWidth - infoBoxHeight;
+        }
+
+        setPosition({
+          top: newTop,
+          left: newLeft,
+        });
       }
-
-      if (e.clientY - nodeWidth <= 0) {
-        newTop = 0;
-      } else if (e.clientY - nodeWidth >= canvasHeight - nodeWidth) {
-        newTop = canvasHeight - nodeWidth;
-      }
-
-      setPosition({
-        top: newTop,
-        left: newLeft,
-      });
-    }
-  };
+    },
+    [canvasRef]
+  );
 
   const handleMouseDown = () => {
     document.onmousemove = handleMouseMove;
@@ -56,6 +74,7 @@ const GraphNode: React.FC<Props> = (props: Props): ReactElement => {
     document.onmousemove = null;
   };
 
+  //side effect for fire node position to connected edges
   useEffect(() => {
     if (props.edgeRef?.current && nodeRef.current) {
       const halfNodeWidth: number = +nodeRef.current.offsetWidth / 2;
@@ -70,6 +89,50 @@ const GraphNode: React.FC<Props> = (props: Props): ReactElement => {
     }
   });
 
+  //the following is to handle boundries of canvas on screen resize (zooming)
+  useEffect(() => {
+    const handleWindowResize = () => {
+      if (
+        nodeRef.current !== null &&
+        canvasRef.current !== null &&
+        infoRef.current !== null
+      ) {
+        const nodeWidth: number = +nodeRef.current.offsetWidth;
+        const canvasWidth: number = +canvasRef.current.offsetWidth;
+        const canvasHeight: number = +canvasRef.current.offsetHeight;
+        const infoBoxHeight: number = +infoRef.current.offsetHeight;
+
+        let newLeft: number = position.left;
+        let newTop: number = position.top;
+
+        if (position.left - nodeWidth <= 0) {
+          newLeft = 0;
+        } else if (position.left + nodeWidth >= canvasWidth) {
+          newLeft = canvasWidth - nodeWidth;
+        }
+
+        if (position.top <= 0) {
+          newTop = 0;
+        } else if (position.top >= canvasHeight - nodeWidth - infoBoxHeight) {
+          newTop = canvasHeight - nodeWidth - infoBoxHeight;
+        }
+
+        setPosition({
+          top: newTop,
+          left: newLeft,
+        });
+      }
+    };
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, [nodeRef, canvasRef, position, infoRef]);
+
+  //side effect for handling out of boundries when zooming in and out
+  useEffect(() => {
+    const event = new CustomEvent<Position>('resize', {});
+    window.dispatchEvent(event);
+  }, [props.zoomPercentage]);
+
   //side effect for centering position on initial render
   useEffect(() => {
     if (canvasRef.current && nodeRef.current) {
@@ -83,39 +146,6 @@ const GraphNode: React.FC<Props> = (props: Props): ReactElement => {
     }
   }, [canvasRef, nodeRef, setPosition]);
 
-  //the following is to handle boundries of canvas on screen resize (zooming)
-  useEffect(() => {
-    const handleWindowResize = () => {
-      if (nodeRef.current !== null && canvasRef.current !== null) {
-        const nodeWidth: number = +nodeRef.current.offsetWidth;
-        const canvasWidth: number = +canvasRef.current.offsetWidth;
-        const canvasHeight: number = +canvasRef.current.offsetHeight;
-
-        let newLeft: number = position.left;
-        let newTop: number = position.top;
-
-        if (position.left - nodeWidth / 2 <= 0) {
-          newLeft = 0;
-        } else if (position.left - nodeWidth / 2 >= canvasWidth - nodeWidth) {
-          newLeft = canvasWidth - nodeWidth;
-        }
-
-        if (position.top - nodeWidth <= 0) {
-          newTop = 0;
-        } else if (position.top >= canvasHeight - nodeWidth) {
-          newTop = canvasHeight - nodeWidth;
-        }
-
-        setPosition({
-          top: newTop,
-          left: newLeft,
-        });
-      }
-    };
-    window.addEventListener('resize', handleWindowResize);
-    return () => window.removeEventListener('resize', handleWindowResize);
-  }, [nodeRef, canvasRef, position]);
-
   return (
     <Container
       isActive={props.isActive}
@@ -127,8 +157,8 @@ const GraphNode: React.FC<Props> = (props: Props): ReactElement => {
     >
       {props.content}
       {props.children}
-      <Information zoomPercentage={props.zoomPercentage}>
-        <div>SP: 5</div>
+      <Information ref={infoRef} zoomPercentage={props.zoomPercentage}>
+        <div>SP: 100</div>
         <div>prev: 1</div>
       </Information>
     </Container>
