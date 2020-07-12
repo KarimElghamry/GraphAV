aa28633088f8924feed437a26abffa52b73f31da
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-12 11:06:47 +0200

    add: create edge input container

---
+++ src/components/CreateEdgeModal/StyledDropdownContainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledDropdownContainer = styled.div`
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+    align-items: center;
+    justify-items: space-between;
+    grid-gap: 40px;
+`;
+
+export default StyledDropdownContainer;
