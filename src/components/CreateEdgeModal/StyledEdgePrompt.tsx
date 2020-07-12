2687f24afa02cab81726f29c9029c7eb1fd3c025
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-12 11:09:44 +0200

    change: padding right to accomodate grid spacing

--- src/components/CreateEdgeModal/StyledEdgePrompt.tsx
+++ src/components/CreateEdgeModal/StyledEdgePrompt.tsx
@@ -4,7 +4,7 @@ const StyledEdgePrompt = styled.div`
     align-items: center;
     justify-items: space-between;
     padding-left: 40px;
-    padding-right: 40px;
+    padding-right: 60px;
     width: 380px;
 
 `;
