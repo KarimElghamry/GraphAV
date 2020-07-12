078529f319ac5fa26bd8a2672a0af0d8ef2c80bf
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-12 11:46:22 +0200

    add: styled container for EdgeModal action buttons

---
+++ src/components/CreateEdgeModal/StyledActionContainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledActionContainer = styled.div`
+    display: grid;
+    grid-template-columns: 120px 120px;
+    justify-content: right;
+    margin:20px;
+    width: 95%;
+`;
+
+export default StyledActionContainer;
