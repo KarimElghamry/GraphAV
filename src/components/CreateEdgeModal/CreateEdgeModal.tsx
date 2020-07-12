70438b018c463b8df3fb2573216ba934948b5203
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-12 11:09:02 +0200

    add: directed property to props

--- src/components/CreateEdgeModal/CreateEdgeModal.tsx
+++ src/components/CreateEdgeModal/CreateEdgeModal.tsx
@@ -4,6 +4,7 @@ import Dropdown from '../common/Dropdown/Dropdown';
 import StyledEdgePrompt from './StyledEdgePrompt';
 import StyledDropdownContainer from './StyledDropdownContainer';
 interface Props {
+    directed: boolean;
     isVisible: boolean,
     onExit: Function,
 }
@@ -13,7 +14,7 @@ const CreateEdgeModal = (props: Props) => {
     return (
         <Modal onExit={() => { }} isVisible={true}>
             <StyledEdgePrompt>
-                <h2>Create undirected edge</h2>
+                <h2>Create {props.directed ? "directed" : "undirected"} edge</h2>
                 <StyledDropdownContainer>
                     From
                         <Dropdown content={["1", "2"]} selectedTile={2} setSelectedTile={() => { }} />
