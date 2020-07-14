5d96f5951967976e1bfd6dbf6a3ee15404396e7e
KarimElghamry <Karimelghamry@gmail.com>
2020-07-14 17:51:13 +0200

    add: ContextMenu/Container

---
+++ src/components/GraphNode/ContextMenu/Container.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: absolute;
+  height: 100px;
+  width: 200px;
+  color: red;
+  border-radius: 10px;
+  left: 50%;
+  top: 50%;
+`;
+
+export default Container;
