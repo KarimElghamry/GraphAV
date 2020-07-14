2cc4c53549e4b6f446b7db80fffd31c222d397a2
KarimElghamry <Karimelghamry@gmail.com>
2020-07-14 17:47:59 +0200

    add: GraphNode/ContextMenu

---
+++ src/components/GraphNode/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const ContextMenu: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ContextMenu;
