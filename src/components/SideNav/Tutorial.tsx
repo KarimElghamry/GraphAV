b8ecdd4465dacb18ce5b924a5218823d234e5c03
KarimElghamry <Karimelghamry@gmail.com>
2020-07-19 14:23:06 +0200

    add: Tutorial component file

---
+++ src/components/SideNav/Tutorial.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const Tutorial: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Tutorial;
