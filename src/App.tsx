0a66c95e487b7dedf8ae1701ae81d0e08a1db8e6
KarimElghamry <Karimelghamry@gmail.com>
2020-07-19 15:17:33 +0200

    add: Tutorial to app level

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import {ThemeProvider} from 'styled-components';
 import themes from './themes';
 import Theme from './models/Theme';
+import Tutorial from './components/Tutorial/Tutorial';
 
 const App: React.FC<{}> = (): ReactElement => {
   const [globalTheme, setGlobalTheme] = useState<Theme>(themes.light);
@@ -10,6 +11,7 @@ const App: React.FC<{}> = (): ReactElement => {
   return (
     <ThemeProvider theme={globalTheme}>
       <Home changeTheme={setGlobalTheme}></Home>
+      <Tutorial />
     </ThemeProvider>
   );
 };
