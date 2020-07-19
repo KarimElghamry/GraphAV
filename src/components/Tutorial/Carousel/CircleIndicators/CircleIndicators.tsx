145f5bd30533adbb1604292f6e53c5d2f8c414f5
KarimElghamry <Karimelghamry@gmail.com>
2020-07-19 15:58:56 +0200

    add: Carousel/CircleIndicator

---
+++ src/components/Tutorial/Carousel/CircleIndicators/CircleIndicators.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const CircleIndicators: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default CircleIndicators;
