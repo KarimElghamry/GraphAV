cbc7179e868d3090abca204a8a03368c98b57095
KarimElghamry <Karimelghamry@gmail.com>
2020-07-19 16:23:54 +0200

    add: onCircleSelect prop to CircleIndicators

--- src/components/Tutorial/Carousel/Carousel.tsx
+++ src/components/Tutorial/Carousel/Carousel.tsx
@@ -12,6 +12,10 @@ interface Props {
 const Carousel: React.FC<Props> = (props: Props): ReactElement => {
   const [currentSelection, setCurrentSelection] = useState<number>(0);
 
+  const onCircleSelect = (index: number) => {
+    setCurrentSelection(index);
+  };
+
   return (
     <Container>
       <Column width="100px">
@@ -22,7 +26,11 @@ const Carousel: React.FC<Props> = (props: Props): ReactElement => {
           EMINEM
         </Row>
         <Row justifyContent="center" style={{height: '10%', width: '100%'}}>
-          <CircleIndicators circlesCount={5} selected={1}></CircleIndicators>
+          <CircleIndicators
+            circlesCount={7}
+            selected={currentSelection}
+            onCircleSelect={onCircleSelect}
+          ></CircleIndicators>
         </Row>
       </Column>
       <Column width="100px">
--- src/components/Tutorial/Carousel/CircleIndicators/CircleIndicators.tsx
+++ src/components/Tutorial/Carousel/CircleIndicators/CircleIndicators.tsx
@@ -4,15 +4,19 @@ import Circle from './Circle';
 interface Props {
   circlesCount: number;
   selected: number;
+  onCircleSelect: (index: number) => void;
 }
 
 const CircleIndicators: React.FC<Props> = (props: Props): ReactElement => {
   return (
     <React.Fragment>
       {[...Array(props.circlesCount)].map((_, index: number) => {
-        console.log('eminem');
         return (
-          <Circle key={index} isSelected={index === props.selected}></Circle>
+          <Circle
+            key={index}
+            isSelected={index === props.selected}
+            onClick={() => props.onCircleSelect(index)}
+          ></Circle>
         );
       })}
     </React.Fragment>
