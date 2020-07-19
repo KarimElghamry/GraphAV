7910ed6d3c30f4b653b48af4162c3b26670127b3
KarimElghamry <Karimelghamry@gmail.com>
2020-07-19 17:40:42 +0200

    fix: modal minimum width

--- src/components/common/Modal/Content.tsx
+++ src/components/common/Modal/Content.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Content = styled.div`
-  min-width: 400px;
+  min-width: 300px;
   min-height: 200px;
   border-radius: 7.5px;
   background-color: ${(props) => props.theme.sidebar.background};
