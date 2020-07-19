64fb87fe0183be18fda61fc79df69e9605986859
KarimElghamry <Karimelghamry@gmail.com>
2020-07-19 15:26:39 +0200

    add: common/Column

---
+++ src/components/common/Column.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Column = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Column;
