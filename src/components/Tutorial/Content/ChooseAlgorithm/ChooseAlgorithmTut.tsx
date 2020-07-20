d7dc9c3f4094c0f267e68cd6feb1a8c4c044f93c
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-20 20:42:36 +0200

    add: choose algorithm tut

--- src/components/Tutorial/Content/ChooseAlgorithm/ChooseAlgorithmTut.tsx
+++ src/components/Tutorial/Content/ChooseAlgorithm/ChooseAlgorithmTut.tsx
@@ -1,8 +1,35 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import Dropdown from '../../../common/Dropdown/Dropdown';
+import HeadingContainer from '../HeadingContainer';
 
 const ChooseAlgorithmTut: React.FC = (): ReactElement => {
+    const [selectedAlgorithm, setAlgorithm] = useState<number>(0);
+    const [selectedStartNode, setStartNode] = useState<number>(0);
+
+    const algorithms: Array<string> = ["DFS", "BFS", "Djikstra"]
+
     return (
         <div>
+            <HeadingContainer>
+                <h1>Select Algorithm</h1>
+                <h4>Choose algorithm then choose the starting vertix for traversal from the tool menu</h4>
+            </HeadingContainer>
+            <div style={{ top: -40, left: -60, position: "absolute", textAlign: 'center' }}>
+                <h4>Algorithm</h4>
+
+                <Dropdown
+                    selectedTile={selectedAlgorithm}
+                    setSelectedTile={setAlgorithm}
+                    content={algorithms}
+                ></Dropdown>
+                <h4>Starting Node</h4>
+
+                <Dropdown
+                    selectedTile={selectedStartNode}
+                    setSelectedTile={setStartNode}
+                    content={['1', '2', '3']}
+                ></Dropdown>
+            </div>
 
         </div>
     );
