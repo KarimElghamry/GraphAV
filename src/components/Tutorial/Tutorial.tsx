552602f288fe31f9ecfbd8188948022916375d3c
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-20 18:44:24 +0200

    add: delete node tutorial component

--- src/components/Tutorial/Tutorial.tsx
+++ src/components/Tutorial/Tutorial.tsx
@@ -1,7 +1,8 @@
-import React, {ReactElement} from 'react';
+import React, { ReactElement } from 'react';
 import Modal from '../common/Modal/Modal';
 import Carousel from './Carousel/Carousel';
-
+import DeleteNodeTut from './Content/DeleteNode/DeleteNodeTut';
+import ContentContainer from './Content/ContentContainer';
 interface Props {
   isVisible: boolean;
   onExit: () => void;
@@ -10,9 +11,9 @@ interface Props {
 const Tutorial: React.FC<Props> = (props: Props): ReactElement => {
   return (
     <Modal isVisible={props.isVisible} onExit={props.onExit}>
-      <div style={{width: '600px', height: '500px'}}>
+      <div style={{ width: '600px', height: '500px' }}>
         <Carousel>
-          <div>1</div>
+          <ContentContainer><DeleteNodeTut /></ContentContainer>
           <div>2</div>
           <div>3</div>
           <div>4</div>
