13a701a7ac8a2944a239e0cf1c72277bfa587bd5
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-20 19:11:33 +0200

    add: tutorial heading container

---
+++ src/components/Tutorial/Content/HeadingContainer.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HeadingContainer = styled.div`
+    position: absolute; 
+    top: -150px;
+    left: -110px;
+    width: 480px;
+`;
+
+export default HeadingContainer;
