ca3ac85b8ccd90e8b0ea5cff0c34a3cbd9149079
KarimElghamry <Karimelghamry@gmail.com>
2020-07-21 14:31:42 +0200

    Updates

---
+++ precache-manifest.18d47a7aa346abe114a108fd4ba63506.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2620339314b935eb4a71530c624dc448",
+    "url": "/GraphAV/index.html"
+  },
+  {
+    "revision": "0f7904c5c2ecdf8fe312",
+    "url": "/GraphAV/static/css/main.39df5605.chunk.css"
+  },
+  {
+    "revision": "ec45620c0117a1c57c61",
+    "url": "/GraphAV/static/js/2.55136a6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GraphAV/static/js/2.55136a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7904c5c2ecdf8fe312",
+    "url": "/GraphAV/static/js/main.5aa960db.chunk.js"
+  },
+  {
+    "revision": "0fe0ffda80151ca027ef",
+    "url": "/GraphAV/static/js/runtime-main.2e1f4f99.js"
+  },
+  {
+    "revision": "69670dd3cc7de1d6a3466f3c4b968764",
+    "url": "/GraphAV/static/media/Ramabhadra.69670dd3.ttf"
+  },
+  {
+    "revision": "1c17663821a16a2a354b6a9fe96a86d7",
+    "url": "/GraphAV/static/media/add.1c176638.svg"
+  },
+  {
+    "revision": "da0de4af439807c2b83611015524ff91",
+    "url": "/GraphAV/static/media/directed.da0de4af.svg"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "/GraphAV/static/media/gh-logo.89c2c067.png"
+  },
+  {
+    "revision": "d2e27e971d844c84603370d54672e1e9",
+    "url": "/GraphAV/static/media/star.d2e27e97.svg"
+  },
+  {
+    "revision": "2611a883dd219a8df0a57766a7bd6c8a",
+    "url": "/GraphAV/static/media/undirected.2611a883.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Graph-Algorithms-Visualizer/precache-manifest.7ec516164286255dbca6a52970bca395.js"
+  "/GraphAV/precache-manifest.18d47a7aa346abe114a108fd4ba63506.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Graph-Algorithms-Visualizer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GraphAV/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
