4ecb81e8e60ece87865c92fb923e91c2add85017
KarimElghamry <Karimelghamry@gmail.com>
2020-07-21 16:38:15 +0200

    add: welcome msg to WelcomeScreen

--- src/components/Tutorial/Content/WelcomeScreen/WelcomeScreen.tsx
+++ src/components/Tutorial/Content/WelcomeScreen/WelcomeScreen.tsx
@@ -6,6 +6,13 @@ import Row from '../../../common/Row';
 const WelcomeScreen: React.FC = (): ReactElement => {
   return (
     <Container>
+      <Row
+        justifyContent="center"
+        margin="30px 10px"
+        style={{fontSize: '30px'}}
+      >
+        Welcome to
+      </Row>
       <GavLogo></GavLogo>
       <Row justifyContent="center" margin="0px 10px" style={{fontSize: '80px'}}>
         GraphAV
