96a941e2090f898f48eb611694cb169bcb84611d
KarimElghamry <Karimelghamry@gmail.com>
2020-07-21 15:08:02 +0200

    add: GraphAV logo to assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import githubLogo from './images/gh-logo.png';
 import GAVLogo from './images/GAV-Logo.png';
+import GraphAVLogo from './images/GraphAV-logo.png';
 import {ReactComponent as starIcon} from './images/star.svg';
 import {ReactComponent as addIcon} from './images/add.svg';
 import {ReactComponent as undirectedIcon} from './images/undirected.svg';
@@ -13,6 +14,7 @@ const assets = {
     undirectedIcon: undirectedIcon,
     directedIcon: directedIcon,
     gavLogo: GAVLogo,
+    graphAVLogo: GraphAVLogo,
   },
 };
 
