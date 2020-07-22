ccd8b4754cea0fb9b2785f88421e8f46e1543c35
KarimElghamry <Karimelghamry@gmail.com>
2020-07-22 17:18:07 +0200

    fix: Authros styling

--- src/components/SideNav/Authors/AuthorTile.tsx
+++ src/components/SideNav/Authors/AuthorTile.tsx
@@ -5,9 +5,14 @@ const AuthorTile = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  margin: 5px 0px 5px 20px;
+  padding: 8px 0px 8px 20px;
   color: white;
   cursor: pointer;
+  transition-duration: 0.3s;
+
+  &:hover {
+    background-color: ${(props) => props.theme.navbar.background};
+  }
 `;
 
 export default AuthorTile;
--- src/components/SideNav/Authors/Container.tsx
+++ src/components/SideNav/Authors/Container.tsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
+import themes from '../../../themes';
 
 const Container = styled.div`
-  background-color: #24292e;
+  background-color: ${(props) =>
+    props.theme.name === 'dark'
+      ? themes.light.navbar.background
+      : themes.dark.navbar.background};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  border: 3px solid ${(props) => props.theme.sidebar.background};
+  margin-top: 40px;
+  transition-duration: 0.3s;
 `;
 
 export default Container;
