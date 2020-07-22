e4553fbd689b600f1942c8793129aa6197c1f924
KarimElghamry <Karimelghamry@gmail.com>
2020-07-22 17:36:09 +0200

    add: open url functionality to Authors

--- src/components/SideNav/Authors/Authors.tsx
+++ src/components/SideNav/Authors/Authors.tsx
@@ -3,17 +3,26 @@ import Container from './Container';
 import GithubLogo from './GithubLogo';
 import AuthorTile from './AuthorTile';
 
+const authorsDetails = {
+  KarimElghamry: 'https://github.com/KarimElghamry',
+  AliAmin: 'https://github.com/Ali-Amin',
+};
+
 const Authors: React.FC = (): ReactElement => {
+  const openUrl = (url: string) => {
+    window.open(url, '_blank')?.focus();
+  };
+
   return (
     <Container>
-      <AuthorTile>
-        <GithubLogo></GithubLogo>
-        KarimElghamry
-      </AuthorTile>
-      <AuthorTile>
-        <GithubLogo></GithubLogo>
-        AliAmin
-      </AuthorTile>
+      {Object.entries(authorsDetails).map((item: [string, string]) => {
+        return (
+          <AuthorTile onClick={() => openUrl(item[1])}>
+            <GithubLogo></GithubLogo>
+            {item[0]}
+          </AuthorTile>
+        );
+      })}
     </Container>
   );
 };
