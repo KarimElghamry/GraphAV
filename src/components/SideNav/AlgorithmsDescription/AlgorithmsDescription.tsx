f18b3119ddefbac924405fbd1b211911ccf1922f
KarimElghamry <Karimelghamry@gmail.com>
2020-07-22 14:10:39 +0200

    add: AlgorithmsDescription file

---
+++ src/components/SideNav/AlgorithmsDescription/AlgorithmsDescription.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const AlgorithmsDescription: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default AlgorithmsDescription;
