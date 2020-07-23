d4c6655202b21fed22b26373e303e67393b774f3
KarimElghamry <Karimelghamry@gmail.com>
2020-07-23 16:16:15 +0200

    fix: starting node info in bfs

--- src/algorithms/bfs.ts
+++ src/algorithms/bfs.ts
@@ -1,76 +1,84 @@
-import asyncTimout from '../helpers/asyncTimout'
+import asyncTimout from '../helpers/asyncTimout';
 import NodeInfo from '../models/NodeInfo';
 
-
 const bfsWrapper = async (
-    adjacencyList: Array<Array<number>>,
-    setVisited: Function,
-    startingNode: number,
-    visualizationSpeed: number,
-    setGraphInfo: Function,
-    setCurrentEdge: Function,
+  adjacencyList: Array<Array<number>>,
+  setVisited: Function,
+  startingNode: number,
+  visualizationSpeed: number,
+  setGraphInfo: Function,
+  setCurrentEdge: Function
 ) => {
+  // init
+  const infoTable: Array<NodeInfo> = adjacencyList.map((neighbours, node) => {
+    return {
+      shortestPath: startingNode === node ? 0 : undefined,
+      previousNode: undefined,
+    } as NodeInfo;
+  });
+  const visited: Array<number> = [];
+  setVisited(visited.slice());
+  await visitNode(
+    setVisited,
+    startingNode,
+    visualizationSpeed,
+    visited,
+    0,
+    -1,
+    infoTable,
+    setGraphInfo
+  ); //visit first node
+  const nodesToExplore: Array<number> = [startingNode];
+  setGraphInfo(infoTable.slice());
 
-    // init
-    const infoTable: Array<NodeInfo> = adjacencyList.map((neighbours, node) => {
-        return { shortestPath: undefined, previousNode: undefined } as NodeInfo
-    })
-    const visited: Array<number> = [];
-    setVisited(visited.slice());
-    await visitNode(setVisited, startingNode, visualizationSpeed, visited, 0, -1, infoTable, setGraphInfo); //visit first node
-    const nodesToExplore: Array<number> = [startingNode];
-    setGraphInfo(infoTable.slice());
+  // bfs
+  while (nodesToExplore.length > 0) {
+    const currentDepthNodes: Array<number> = nodesToExplore.slice();
 
-    // bfs
-    while (nodesToExplore.length > 0) {
+    for (const nodeToExplore of currentDepthNodes) {
+      for (const nodeToVisit of adjacencyList[nodeToExplore]) {
+        const newDepth: number =
+          (infoTable[nodeToExplore].shortestPath ?? 0) + 1; //TODO: replace 1 with weight of nodeToVisit edge
+        if (!visited.includes(nodeToVisit)) {
+          await visitNode(
+            setVisited,
+            nodeToVisit,
+            visualizationSpeed,
+            visited,
+            newDepth,
+            nodeToExplore,
+            infoTable,
+            setGraphInfo
+          );
+          setCurrentEdge([nodeToExplore, nodeToVisit]);
+          const nodeInfo: NodeInfo = {
+            shortestPath: newDepth,
+            previousNode: nodeToExplore,
+          };
+          infoTable[nodeToVisit] = nodeInfo;
+          setGraphInfo(infoTable.slice());
 
-        const currentDepthNodes: Array<number> = nodesToExplore.slice();
-
-        for (const nodeToExplore of currentDepthNodes) {
-            for (const nodeToVisit of adjacencyList[nodeToExplore]) {
-                const newDepth: number = (infoTable[nodeToExplore].shortestPath ?? 0) + 1; //TODO: replace 1 with weight of nodeToVisit edge
-                if (!visited.includes(nodeToVisit)) {
-                    await visitNode(
-                        setVisited,
-                        nodeToVisit,
-                        visualizationSpeed,
-                        visited,
-                        newDepth,
-                        nodeToExplore,
-                        infoTable,
-                        setGraphInfo,
-                    );
-                    setCurrentEdge([nodeToExplore, nodeToVisit]);
-                    const nodeInfo: NodeInfo = { shortestPath: newDepth, previousNode: nodeToExplore };
-                    infoTable[nodeToVisit] = nodeInfo;
-                    setGraphInfo(infoTable.slice());
-
-                    nodesToExplore.push(nodeToVisit)
-                }
-            }
-            nodesToExplore.shift()
+          nodesToExplore.push(nodeToVisit);
         }
+      }
+      nodesToExplore.shift();
     }
-}
+  }
+};
 
 const visitNode = async (
-    setVisited: Function,
-    node: number,
-    visualizationSpeed: number,
-    visited: Array<number>,
-    depth: number,
-    prevNode: number,
-    infoTable: Array<NodeInfo>,
-    setGraphInfo: Function,
+  setVisited: Function,
+  node: number,
+  visualizationSpeed: number,
+  visited: Array<number>,
+  depth: number,
+  prevNode: number,
+  infoTable: Array<NodeInfo>,
+  setGraphInfo: Function
 ) => {
-    await asyncTimout(visualizationSpeed)
-    visited.push(node);
-    setVisited(visited.slice())
-
-
-
-}
-
-
+  await asyncTimout(visualizationSpeed);
+  visited.push(node);
+  setVisited(visited.slice());
+};
 
 export default bfsWrapper;
