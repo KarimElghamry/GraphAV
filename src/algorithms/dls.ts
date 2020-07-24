cf7755ee4f6477fa7223a1d4df818412ad3c7f64
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-24 15:05:14 +0200

    add: dls logic

--- src/algorithms/dls.ts
+++ src/algorithms/dls.ts
@@ -1,24 +1,55 @@
 import helpers from "../helpers";
 
+const visited: Array<number> = [];
+
 const dlsWrapper = async (
   adjacencyList: Array<Array<number>>,
   startingNode: number,
   setVisited: Function,
   visualizationSpeed: number,
   setCurrentEdge: Function,
-  previousNode: number,
   maxDepth: number
-) => {};
+) => {
+  await dls(
+    adjacencyList,
+    startingNode,
+    setVisited,
+    visualizationSpeed,
+    setCurrentEdge,
+    0,
+    maxDepth,
+    0
+  );
+};
 
 const dls = async (
   adjacencyList: Array<Array<number>>,
-  startingNode: number,
+  node: number,
   setVisited: Function,
   visualizationSpeed: number,
   setCurrentEdge: Function,
   previousNode: number,
   maxDepth: number,
   currentDepth: number
-) => {};
+) => {
+  if (visited.includes(node)) return;
+  if (currentDepth === maxDepth) return;
+  visited.push(node);
+  setVisited(visited.slice());
+  setCurrentEdge([previousNode, node]);
+  await helpers.asyncTimout(visualizationSpeed);
+
+  for (const neighbour of adjacencyList[node])
+    await dls(
+      adjacencyList,
+      neighbour,
+      setVisited,
+      visualizationSpeed,
+      setCurrentEdge,
+      node,
+      maxDepth,
+      currentDepth + 1
+    );
+};
 
 export default dlsWrapper;
