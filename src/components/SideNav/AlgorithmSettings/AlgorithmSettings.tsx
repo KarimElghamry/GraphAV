dc028346824d5dc3bf0336c7f4b4ffb7f96b5a4a
KarimElghamry <Karimelghamry@gmail.com>
2020-07-24 15:37:15 +0200

    move: InputNumber to its own component

--- src/components/SideNav/AlgorithmSettings/AlgorithmSettings.tsx
+++ src/components/SideNav/AlgorithmSettings/AlgorithmSettings.tsx
@@ -1,25 +1,16 @@
 import React, {ReactElement, useState} from 'react';
 import ItemText from '../ItemText';
+import Row from '../../common/Row';
+import InputNumber from './InputNumber/InputNumber';
 
 const AlgorithmSettings: React.FC = (): ReactElement => {
   const [temp, setTemp] = useState<number>(0);
-  const handleTextInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    if (e.currentTarget.validity.valid) {
-      setTemp(
-        e.currentTarget.value.length === 0 ? 0 : parseInt(e.currentTarget.value)
-      );
-    }
-  };
-
   return (
     <div>
       <ItemText>Max Depth</ItemText>
-      <input
-        type="text"
-        pattern="[0-9]*"
-        onChange={handleTextInputChange}
-        value={temp}
-      ></input>
+      <Row justifyContent="center">
+        <InputNumber value={temp} setValue={setTemp}></InputNumber>
+      </Row>
     </div>
   );
 };
---
+++ src/components/SideNav/AlgorithmSettings/InputNumber/InputNumber.tsx
@@ -0,0 +1,27 @@
+import React, {ReactElement} from 'react';
+
+interface Props {
+  value: number;
+  setValue: (value: number) => void;
+}
+
+const InputNumber: React.FC<Props> = (props: Props): ReactElement => {
+  const handleTextInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.validity.valid) {
+      props.setValue(
+        e.currentTarget.value.length === 0 ? 0 : parseInt(e.currentTarget.value)
+      );
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      pattern="[0-9]*"
+      onChange={handleTextInputChange}
+      value={props.value}
+    ></input>
+  );
+};
+
+export default InputNumber;
