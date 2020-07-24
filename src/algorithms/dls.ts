92ebec049f8939e0ccad89dc8964e6079734ca89
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-24 15:00:38 +0200

    add: dls skelaton

---
+++ src/algorithms/dls.ts
@@ -0,0 +1,24 @@
+import helpers from "../helpers";
+
+const dlsWrapper = async (
+  adjacencyList: Array<Array<number>>,
+  startingNode: number,
+  setVisited: Function,
+  visualizationSpeed: number,
+  setCurrentEdge: Function,
+  previousNode: number,
+  maxDepth: number
+) => {};
+
+const dls = async (
+  adjacencyList: Array<Array<number>>,
+  startingNode: number,
+  setVisited: Function,
+  visualizationSpeed: number,
+  setCurrentEdge: Function,
+  previousNode: number,
+  maxDepth: number,
+  currentDepth: number
+) => {};
+
+export default dlsWrapper;
