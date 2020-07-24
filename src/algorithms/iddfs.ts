ea7826ed1d07d1f607d2bde5749935f3274e4145
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-24 15:14:24 +0200

    add: iddfs skelaton

---
+++ src/algorithms/iddfs.ts
@@ -0,0 +1,5 @@
+import helpers from "../helpers/asyncTimout";
+
+const iddfsWrapper = async () => {};
+
+export default iddfsWrapper;
--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,13 +1,15 @@
-import dfsWrapper from './dfs';
-import dijkstra from './dijkstra';
-import bfs from './bfs';
-import bellmanFord from './bellmanFord';
+import dfsWrapper from "./dfs";
+import dijkstra from "./dijkstra";
+import bfs from "./bfs";
+import bellmanFord from "./bellmanFord";
+import iddfs from "./iddfs";
 
 const algorithms = {
   dfs: dfsWrapper,
   dijkstra: dijkstra,
   bfs: bfs,
-  bellmanFord: bellmanFord
+  bellmanFord: bellmanFord,
+  iddfs: iddfs,
 };
 
 export default algorithms;
