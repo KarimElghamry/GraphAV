cf9eaee01f28429f7fd50b8af0d8d3744e57dc2c
KarimElghamry <Karimelghamry@gmail.com>
2020-07-24 15:28:51 +0200

    add: input option to AlgorithmSettings

--- src/components/SideNav/AlgorithmSettings/AlgorithmSettings.tsx
+++ src/components/SideNav/AlgorithmSettings/AlgorithmSettings.tsx
@@ -1,10 +1,25 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useState} from 'react';
 import ItemText from '../ItemText';
 
 const AlgorithmSettings: React.FC = (): ReactElement => {
+  const [temp, setTemp] = useState<number>(0);
+  const handleTextInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.validity.valid) {
+      setTemp(
+        e.currentTarget.value.length === 0 ? 0 : parseInt(e.currentTarget.value)
+      );
+    }
+  };
+
   return (
     <div>
       <ItemText>Max Depth</ItemText>
+      <input
+        type="text"
+        pattern="[0-9]*"
+        onChange={handleTextInputChange}
+        value={temp}
+      ></input>
     </div>
   );
 };
