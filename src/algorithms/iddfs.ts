e46253bc59bf8cee643a80d01d85a6806a96e7b5
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-24 15:26:44 +0200

    add: dfs logic to iddfs

--- src/algorithms/iddfs.ts
+++ src/algorithms/iddfs.ts
@@ -1,5 +1,55 @@
-import helpers from "../helpers/asyncTimout";
+import asyncTimout from "../helpers/asyncTimout";
 
-const iddfsWrapper = async () => {};
+const visited: Array<number> = []; // frontier
+
+const iddfsWrapper = async (
+  adjacencyList: Array<Array<number>>,
+  startingNode: number,
+  setVisited: Function,
+  visualizationSpeed: number,
+  setCurrentEdge: Function
+) => {
+  setVisited([]);
+  await iddfs(
+    adjacencyList,
+    startingNode,
+    setVisited,
+    visualizationSpeed,
+    setCurrentEdge,
+    0,
+    0,
+    0
+  );
+};
+
+const iddfs = async (
+  adjacencyList: Array<Array<number>>,
+  node: number,
+  setVisited: Function,
+  visualizationSpeed: number,
+  setCurrentEdge: Function,
+  previousNode: number,
+  maxDepth: number,
+  currentDepth: number
+) => {
+  if (visited.includes(node)) return;
+  if (currentDepth === maxDepth) return;
+  visited.push(node);
+  setVisited(visited.slice());
+  setCurrentEdge([previousNode, node]);
+  asyncTimout(visualizationSpeed);
+
+  for (const neighbour of adjacencyList[node])
+    await iddfs(
+      adjacencyList,
+      neighbour,
+      setVisited,
+      visualizationSpeed,
+      setCurrentEdge,
+      node,
+      maxDepth,
+      currentDepth + 1
+    );
+};
 
 export default iddfsWrapper;
