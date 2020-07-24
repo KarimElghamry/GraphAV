28448d1804d527dfc1f1f352574afec9bf5238c2
Ali-Amin <ali.m.amin98@gmail.com>
2020-07-24 18:23:14 +0200

    add: dls description

--- src/components/SideNav/AlgorithmsDescription/AlgorithmsDescription.tsx
+++ src/components/SideNav/AlgorithmsDescription/AlgorithmsDescription.tsx
@@ -1,6 +1,6 @@
-import React, {ReactElement} from 'react';
-import Container from './Container';
-import Algorithms from '../../../models/Algorithms';
+import React, { ReactElement } from "react";
+import Container from "./Container";
+import Algorithms from "../../../models/Algorithms";
 
 interface Props {
   selectedAlgorithm: Algorithms;
@@ -9,15 +9,19 @@ interface Props {
 const descriptionText = new Map<Algorithms, string>([
   [
     Algorithms.dfs,
-    'DFS prioritizes the depth while traversing and does not find the shortest path',
+    "DFS prioritizes the depth while traversing and does not find the shortest path",
   ],
   [
     Algorithms.bfs,
-    'BFS traverses the graph level by level and can find the shortest paths',
+    "BFS traverses the graph level by level and can find the shortest paths",
   ],
   [
     Algorithms.dijkstra,
-    'Dijkstra guarantees to find the shortest path from the starting node to the others',
+    "Dijkstra guarantees to find the shortest path from the starting node to the others",
+  ],
+  [
+    Algorithms.dls,
+    "DLS can be used when the depth of the shallowest goal of a problem is known",
   ],
 ]);
 
