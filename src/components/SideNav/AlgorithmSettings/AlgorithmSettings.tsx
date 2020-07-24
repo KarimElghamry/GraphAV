5bd15c356c26c2e751df92835f204f61d9f426e4
KarimElghamry <Karimelghamry@gmail.com>
2020-07-24 15:02:20 +0200

    add: AlgorithmSettings component file

---
+++ src/components/SideNav/AlgorithmSettings/AlgorithmSettings.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+
+const AlgorithmSettings: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default AlgorithmSettings;
